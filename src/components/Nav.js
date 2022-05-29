ebb8df962b667dae429fdf1e74415d177afe1d4e
Abhishek Ajay <abhishekaxaan@gmail.com>
2022-05-29 22:20:10 +0530

    > Logo as Link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import LogoWhite from '../images/famed-logo-white.png'
 import LogoColored from '../images/famed-logo-colored.png'
 // import { useState } from 'react'
@@ -14,13 +15,15 @@ const Nav = ({ colorLogo, setShowModal, setIsSignUp }) => {
 
       return (
             <nav>
-                  <div className="logo-container">
-                        <img
-                              className="logo"
-                              src={colorLogo ? LogoColored : LogoWhite}
-                              alt=""
-                        />
-                  </div>
+                  <Link to="/">
+                        <div className="logo-container">
+                              <img
+                                    className="logo"
+                                    src={colorLogo ? LogoColored : LogoWhite}
+                                    alt=""
+                              />
+                        </div>
+                  </Link>
                   <button
                         className="navBtn"
                         onClick={handleClick}
--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// import { useState } from 'react'
 import Nav from '../components/Nav'
 
 const Onboarding = () => {
